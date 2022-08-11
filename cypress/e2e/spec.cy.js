e78fb3bdbf62aa1feebccfa351f0a1351e3fc747
Rocky <25568640+rockindahizzy@users.noreply.github.com>
2022-08-11 09:32:50 -0500

    Add global function to context

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
+  it('works', function() {
+    this.sayHello()
     cy.visit('https://example.cypress.io')
   })
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,9 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+before(function () {
+  this.sayHello = () => {
+    console.log('Hello Global Function')
+  }
+})
